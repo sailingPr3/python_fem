af2ce3b706383eb1b5ed6fcf56fc1fb21eb75f33
mastraa <mastraa@gmail.com>
2016-04-19 00:24:00 +0200

    bug fixing
    a livello matematico funziona, ci deve essere qualche errore fisico
    perché al momento F[0] e F[18] sono praticamente nulli e non dovrebbe
    essere così, inoltre ci sono 4 ordini di grandezza tra i due (e-13
    e-18) e dovrebbero essere praticamente coincidenti

--- fem.py
+++ fem.py
@@ -32,25 +32,30 @@ def switch(nm_mat, ls_perm):
 			switched[:,i] = tmp[:,ls_perm[i]]
 	return switched
 
-def beam_rigidity(E,J,l,theta = 0):
+def beam_rigidity(E,J,A,l,theta = 0):
 	M = 12. * E * J / (l ** 3)
 	T = 6. * E * J / (l ** 2)
 	S = 2. * E * J / (l)
+	N = E * A / l
 	mat = np.matrix(
 		[
-		[M, T, -M, T],
-		[T, 2*S, -T, S],
-		[-M, -T, M, -T],
-		[T,S,-T, 2*S]])
+		[N,0,0,-N,0,0],
+		[0,M, T, 0,-M, T],
+		[0,T, 2*S, 0,-T, S],
+		[-N,0,0,N,0,0],
+		[0,-M, -T, 0,M, -T],
+		[0,T,S,0,-T, 2*S]])
 	lx = cos(theta)
 	mx = sin(theta)
 	ly = cos(np.pi/2 + theta)
 	my = sin(np.pi/2 + theta)
 	T = np.matrix([
-		[lx,mx,0,0],
-		[ly,my,0,0],
-		[0,0,lx,mx],
-		[0,0,ly,my]])
+		[lx,mx,0,0,0,0],
+		[ly,my,0,0,0,0],
+		[0,0,1,0,0,0],
+		[0,0,0,lx,mx,0],
+		[0,0,0,ly,my,0],
+		[0,0,0,0,0,1]])
 	if theta:
 		mat = T.transpose() * mat * T
 	return mat
@@ -60,39 +65,35 @@ def K_albero(ls_mat):
 		print("Expected 7 matrices")
 		return 0
 
-	K = np.matrix(np.zeros((16,16)),dtype=float)
+	K = np.matrix(np.zeros((19,19)),dtype=float)
 	# First matrix
-	K[0:4,0:4] = ls_mat[0]
+	K[0:6,0:6] = ls_mat[0]
 
 	# Second
-	K[2:6,2:6] += ls_mat[1]
+	K[3:9,3:9] += ls_mat[1]
 
 	# Third
-	K[4:6,4:6] += ls_mat[2][0:2,0:2]
-	K[4:6,7:9] += ls_mat[2][0:2,2:4]
-	K[7:9,4:6] += ls_mat[2][2:4,0:2]
-	K[7:9,7:9] += ls_mat[2][2:4,2:4]
+	K[6:12,6:12] += ls_mat[2]
 
-	# Fourth
-	K[7:9,7:9] += ls_mat[3][0:2,0:2]
-	K[7:9,10:12] += ls_mat[3][0:2,2:4]
-	K[10:12,7:9] += ls_mat[3][2:4,0:2]
-	K[10:12,10:12] += ls_mat[3][2:4,2:4]
+	#Fourth
+	K[9:15,9:15] += ls_mat[3]
 
 	# Fifth
-	mat_5 = switch(ls_mat[4],[1,0])
-	K[5:7,5:7] += mat_5[0:2,0:2]
-	K[5:7,[12,14]] += mat_5[0:2,2:4]
-	K[[12,14],5:7] += mat_5[2:4,0:2]
-	K[[12,12,14,14],[12,14,12,14]] += mat_5[2:4,2:4].reshape((1,4))
+	K[6:9,6:9]+=ls_mat[4][0:3,0:3]
+	K[6:9,15:18]+=ls_mat[4][0:3,3:6]
+	K[15:18,6:9]+=ls_mat[4][3:6,0:3]
+	K[15:18,15:18]+=ls_mat[4][3:6,3:6]
 
 	# Sixth
-	K[9:11,9:11] += ls_mat[5][0:2,0:2]
-	K[9:11,12:14] += ls_mat[5][0:2,2:4]
-	K[12:14,9:11] += ls_mat[5][2:4,0:2]
-	K[12:14,12:14] += ls_mat[5][2:4,2:4]
+	K[12:14,12:14] += ls_mat[5][0:2,0:2]
+	K[12:14,15:17] += ls_mat[5][0:2,2:4]
+	K[15:17,12:14] += ls_mat[5][2:4,0:2]
+	K[15:17,15:17] += ls_mat[5][2:4,2:4]
 
 	# Seventh
-	K[[12,12,15,15],[12,15,12,15]] += ls_mat[6].reshape((1,4))
+	K[15,15]+=ls_mat[6][0,0]
+	K[18,18]+=ls_mat[6][1,1]
+	K[15,18]+=ls_mat[6][0,1]
+	K[18,15]+=ls_mat[6][1,0]
 
 	return K
--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from fem import beam_rigidity, truss_rigidity, K_albero
 import numpy as np
 
 E = np.array([80,80,80,80,80,210,210],dtype = float) * 1000 # 1 - 7
-J1 = np.array([0,0,0,0,729.2],dtype=float) # 5
-J2 = np.array([69,69,69,69,0],dtype=float)*10000 # 1 - 4
+J1 = np.array([0,0,0,0,1042],dtype=float) # 5
+J2 = np.array([3.15,3.15,3.15,3.15,0],dtype=float)*100000 # 1 - 4
 J = J1 + J2
-A = np.array([50.3,50.3]) # 6,7
+A = np.array([1534,1534,1534,1534,500,7.1,7.1]) # 6,7
 l = np.array([1000,2000,3000,1000,750,0,3000],dtype=float) # 1-5,7
 l[5] = np.sqrt(np.square(l[2]) + np.square(l[4]))
+
+F=np.zeros(19)
+f=np.zeros(19)
+
+F[4]=-100
+F[10]=1000
 
 # Our rotation angles
 theta = -np.arccos(l[2]/l[5]) - np.radians(180)
@@ -16,11 +22,21 @@ alpha = np.radians(270)
 ls_mat = []
 for i in range(5):
     ls_mat.append(
-        beam_rigidity(E[i],J[i],l[i]) if i!=4 else beam_rigidity(E[i],J[i],l[i],alpha)
+        beam_rigidity(E[i],J[i],A[i],l[i]) if i!=4 else beam_rigidity(E[i],J[i],l[i],alpha)
         )
-ls_mat.append(truss_rigidity(E[5],A[0],l[5],theta))
-ls_mat.append(truss_rigidity(E[6],A[1],l[6]))
+ls_mat.append(truss_rigidity(E[5],A[5],l[5],theta))
+ls_mat.append(truss_rigidity(E[6],A[6],l[6]))
+
 K = K_albero(ls_mat)
+K_r=np.linalg.inv(K[2:19,2:19])
+
+f[2:19]=K_r.dot(F[2:19])
+F=K.dot(f)
+
+np.savetxt("F.txt",F,fmt="%.4e")
+np.savetxt("Kr_albero.txt",K_r,fmt="%.4e")
+np.savetxt("f_albero.txt",f,fmt="%.4e")
 np.savetxt("K_albero.txt",K,fmt="%.4e")
 
-print K
+print f
+print F
